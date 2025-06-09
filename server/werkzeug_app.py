971d58d7ff5f1206a0af65f7e54e10017e1b512b
katanujoy <katanujoyy99@gmail.com>
2025-06-09 14:45:27 +0300

    Werkzeug WSGI app is running perfectly

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,15 @@
-#!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+from werkzeug.serving import run_simple
+
+def application(environ, start_response):
+    request = Request(environ)
+    print(f'This web server is running at {request.remote_addr}')
+    response = Response('A WSGI generated this response!')
+    return response(environ, start_response)
+
+if __name__ == '__main__':
+    run_simple(
+        hostname='localhost',
+        port=5555,
+        application=application
+    )
